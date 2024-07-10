04089b85eb998784e7b0a9ba581b22b9cf5adeba
arthurbarcala <arthurfbarcalars@gmail.com>
2024-07-10 07:58:44 -0300

    Initial Commit

---
+++ poc-video/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  poc-video
+//
+//  Created by Arthur Silva on 10/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
